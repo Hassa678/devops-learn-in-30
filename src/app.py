09ec1d0ef0ff92e5cab3b5d0b6780f253dcd9f4d
Hassa678 <132628935+Hassa678@users.noreply.github.com>
2025-07-09 18:14:28 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World-devops-taj1'
+    return 'Hello, World-devops-taj12'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
