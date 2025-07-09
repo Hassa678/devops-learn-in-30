1f4cb154eb7d1acf2277c81859c01d3c393cdac2
Hassa678 <hassantaj00@gmail.com>
2025-07-09 18:30:45 +0100

    last3

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World-devops-taj12'
+    return 'Hello, World-devops-taj123'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
